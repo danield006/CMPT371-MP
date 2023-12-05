0a3ae68c574de41b9e3e44e7c219b0d36199bcca
Luciano <lmt12@sfu.ca>
2023-12-04 21:36:17 -0800

    proxy fixes

--- TestingClient.py
+++ TestingClient.py
@@ -8,12 +8,12 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 request = """GET /test.html HTTP/1.1
-Host: localhost:12000
+Host: localhost:13000
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0
 Accept: text/html
 Connection: keep-alive
 Content-Length: 0
-If-Modified-Since: Sun, 10 Nov 2023 02:01:00 GMT"""
+If-Modified-Since: Sun, 10 Jan 2024 02:01:00 GMT"""
 
 clientSocket.send(request.encode('utf-8'))
 
